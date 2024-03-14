8d7de811a1699608ad2bba01f2e4dcaf1e5ca456
Hugo <hugo.castro@ibm.com>
2024-03-14 14:38:30 -0300

    add filters in template

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,22 +1,21 @@
 from django.http import (HttpResponse, HttpResponseNotFound,
                          HttpResponseRedirect)
 from django.shortcuts import render
-from django.template.loader import render_to_string
 from django.urls import reverse
 
 monthly_challenges: dict[str, str] = {
-    "january": "[Jan] Eat no meat for the entire month!",
-    "february": "[Feb] Walk for at least 20 minutes every day!",
-    "march": "[Mar] Learn Django for at least 20 minutes every day!",
-    "april": "[Apr] Eat no meat for the entire month!",
-    "may": "[May] Walk for at least 20 minutes every day!",
-    "june": "[Jun] Learn Django for at least 20 minutes every day!",
-    "july": "[Jul] Eat no meat for the entire month!",
-    "august": "[Aug] Walk for at least 20 minutes every day!",
-    "september": "[Sep] Learn Django for at least 20 minutes every day!",
-    "october": "[Oct] Eat no meat for the entire month!",
-    "november": "[Nov] Walk for at least 20 minutes every day!",
-    "december": "[Dec] Learn Django for at least 20 minutes every day!"
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
 }
 
 def index(request) -> HttpResponse:
@@ -47,11 +46,13 @@ def monthly_challenge_by_number(request, month) -> HttpResponse:
     return HttpResponseRedirect(redirect_path)
 
 
-def monthly_challenge(request, month) -> HttpResponse:
+def monthly_challenge(request, month: str) -> HttpResponse:
     response_text: str = "<h1>This month is not supported!</h1>"
     try:
         response_text: str = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "month": month,
+            "text": response_text
+        })
     except:
-        return HttpResponseNotFound(response_data)
+        return HttpResponseNotFound(response_text)
